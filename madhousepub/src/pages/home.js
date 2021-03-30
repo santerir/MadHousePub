db9dae21db9a10ddb9c43aa25d16247b093d3010
Santeri Räisänen <santeri.raisanen@solinor.com>
2021-03-31 02:42:18 +0300

    infinite scroll

--- madhousepub/src/pages/home.js
+++ madhousepub/src/pages/home.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState, useRef } from 'react'
 import { Link, useParams, useHistory } from 'react-router-dom'
-import { range, debounce, reduce, clone } from 'lodash';
+import { range, debounce, reduce, noop, map, some } from 'lodash';
+
+import ScrollSnap from 'scroll-snap'
 
 import SVGFilters from '../components/svg-filters'
 import { Hole1, Hole2, Hole3, Hole4, Hole5, Hole6, Hole7, Hole8, Inf } from '../components/svgs'
@@ -20,63 +22,122 @@ export default ({ }) => {
     const [break_points, setBreak_points] = useState([0, 0]);
     const [sizeChanged, setsizeChanged] = useState(false);
     const [resizing, setResizing] = useState(false);
+    const [snapped, setSnapped] = useState(false);
 
     const content_area = useRef();
+    const snapper = useRef();
+    const intersector = useRef();
+
+    const frame1 = useRef();
+    const frame2 = useRef();
+    const frame3 = useRef();
+    const frame4 = useRef();
 
     const history = useHistory();
 
     const getWidth = () => content_area.current.scrollWidth;
 
-    const on_scroll = (e) => {
-        if (resizing) {
-            return;
-        }
-        if (!content_area.current) {
-            return;
-        }
+
+    const isMobile = () => { return ('ontouchstart' in document.documentElement); }
+
+    const on_snapped = () => {
+        setSnapped(false);
         let scroll_pos = content_area.current.scrollLeft
         let pane_size = break_points[1]
-        let pos = Math.round(scroll_pos / pane_size);
+        let pos = Math.round(scroll_pos / pane_size) - 1;
+        if (pos < 0) {
+            content_area.current.scrollLeft = break_points[SCROLL_POINTS];
+            setCurrent_pane(SCROLL_POINTS - 1);
+            return;
+        }
+        if (pos > (SCROLL_POINTS - 1)) {
+            content_area.current.scrollLeft = break_points[1];
+            setCurrent_pane(0);
+            return
+        }
         setCurrent_pane(pos);
     }
 
+    const on_snap = () => {
+        setSnapped(true);
+    }
+
+    // const on_intersect = (entries) => {
+    //     let is_snapped = reduce(entries, (acc, entry) => {
+    //         // Each entry describes an intersection change for one observed
+    //         // target element:
+    //         //   entry.boundingClientRect
+    //         //   entry.intersectionRatio
+    //         //   entry.intersectionRect
+    //         //   entry.isIntersecting
+    //         //   entry.rootBounds
+    //         //   entry.target
+    //         //   entry.time
+    //         if (entry.isIntersecting) {
+    //             return true;
+    //         }
+    //         return acc;
+    //     }, false);
+
+    //     if (is_snapped) {
+    //         return setSnapped(true);
+    //     }
+    // }
+
     const populate_breakpoints = () => {
         var total = getWidth();
-        var arr = range(SCROLL_POINTS);
-        arr = arr.map((n) => ((total / SCROLL_POINTS) * n))
+        var arr = range(SCROLL_POINTS + 2);
+        let x = 0;
+        if (isMobile()) {
+            x = -5;
+        }
+        arr = arr.map((n) => ((total / (SCROLL_POINTS + 2)) * (n) + x));
         setBreak_points(arr)
     }
 
+    const getOn_scroll = () => {
+        if (isMobile()) {
+            return on_scroll;
+        }
+        return noop;
+    }
+
+
+    const on_scroll = () => {
+
+        let scroll_pos = content_area.current.scrollLeft;
+
+        let pane_size = break_points[1]
+        let pos = Math.round(scroll_pos / pane_size) - 1;
+
+
+        if (pos < 0) {
+            content_area.current.scrollTo({ left: break_points[SCROLL_POINTS], top: 0 });
+            setCurrent_pane(SCROLL_POINTS - 1);
+            return;
+        }
+        if (pos > (SCROLL_POINTS - 1)) {
+            content_area.current.scrollTo({ left: break_points[1], top: 0 });
+            setCurrent_pane(0);
+            return
+        }
+
+        setCurrent_pane(pos);
+    }
 
     const go_to = (i) => {
         return () => {
-            content_area.current.scrollTo({ left: break_points[i], top: 0, behaviour: "smooth" })
+            content_area.current.scrollTo({ left: break_points[i + 1], top: 0, behavior: "smooth" })
         }
     }
 
 
     const scrollHorizontally = (e) => {
-        // var delta = Math.min(1, Math.max(-1, (e.deltaX + e.deltaY))) * 40;
-        // content_area.current.scrollLeft -= delta
-        // e.preventDefault();
+        e.preventDefault();
+        var delta = Math.min(1, Math.max(-1, (- e.deltaX - e.deltaY))) * 17;
+        content_area.current.scrollLeft -= delta
     }
 
-
-    useEffect(() => {
-
-        let handle_resize = () => {
-            setResizing(false);
-            setsizeChanged(true);
-        }
-
-        populate_breakpoints();
-        window.addEventListener("resize", debounce(() => { setResizing(true) }, 300, {
-            'leading': true
-        }
-        ));
-        window.addEventListener("resize", debounce(handle_resize, 500));
-
-    }, []);
 
     useEffect(() => {
 
@@ -91,24 +152,74 @@ export default ({ }) => {
 
     useEffect(() => {
 
-        content_area.current.scrollTo({ left: break_points[current_pane], top: 0 })
+        content_area.current.scrollTo({ left: break_points[current_pane + 1], top: 0 })
 
     }, [break_points])
 
     useEffect(() => {
 
-        content_area.current.addEventListener('wheel', scrollHorizontally, false);
+        if (snapped) {
+            on_snapped();
+        }
 
-        return (() => {
-            content_area.current.removeEventListener('wheel', scrollHorizontally)
-        })
+    }, [snapped])
+
+    useEffect(() => {
+
+        populate_breakpoints();
+
+        if (!isMobile()) {
+            bindScrollSnap()
+            content_area.current.addEventListener('wheel', scrollHorizontally, false);
+
+            let handle_resize = () => {
+                setResizing(false);
+                setsizeChanged(true);
+            }
+
+            window.addEventListener("resize", debounce(() => { setResizing(true) }, 300, {
+                'leading': true
+            }
+            ));
+            window.addEventListener("resize", debounce(handle_resize, 500));
+
+            return (() => {
+                content_area.current.removeEventListener('wheel', scrollHorizontally)
+            })
+        }
+
+
+        if (isMobile) {
+            // let options = {
+            //     root: document.querySelector('#content-area'),
+            //     rootMargin: '0px',
+            //     threshold: .7
+            // }
+
+            // intersector.current = new IntersectionObserver(on_intersect, options);
+            // intersector.current.observe(frame1.current);
+            // intersector.current.observe(frame2.current);
+            // intersector.current.observe(frame3.current);
+            // intersector.current.observe(frame4.current);
+
+            // content_area.current.addEventListener('touchmove', throttle(on_scroll, 200));
+
+        }
 
     }, [])
 
-
-
-
-    // TODO: debounce and filter scroll event
+    const bindScrollSnap = () => {
+        const snapElement = new ScrollSnap(content_area.current, {
+            snapDestinationX: '100%',
+            snapStop: false,
+            duration: 100,
+            timeout: 100,
+            threshold: 0.4,
+            easing: t => t * (2 - t)
+        });
+        snapper.current = snapElement;
+        snapper.current.bind(on_snap)
+    }
 
     return (
         <>
@@ -124,11 +235,52 @@ export default ({ }) => {
                     <img className="pub-logo" src="/img/MHPLogo.svg" />
                     <h3> {CONTENT_PANES[current_pane].name} </h3>
                 </div>
-                <div className="content-area" id="content-area" ref={content_area} onScroll={debounce(on_scroll, 200)}>
+                <div className="content-area" id="content-area" ref={content_area} onScroll={getOn_scroll()}>
                     <div className="dots">
                         {range(SCROLL_POINTS).map((i) => (<label onClick={go_to(i)} className={current_pane == i ? "active" : "inactive"}></label>))}
                     </div>
-                    <div className="content-field s2020">
+                    <div className="content-field s2021" ref={frame1}>
+                        <div class="article-link">
+                            <Link to="/fluctuants">
+                                <h1>FLUCTUANTS</h1>
+                            </Link>
+
+                            <div s-only="dsk" className="spacer"></div>
+                            <div className="hole-container">
+                                <Hole5 />
+                            </div>
+                        </div>
+                        <div class="article-link">
+                            <Link to="/ryhman-jasenet">
+                                <h1>Ryhmän jäsenet</h1>
+                            </Link>
+                            <div s-only="dsk" className="spacer"></div>
+                            <div className="hole-container">
+                                <Hole6 />
+                            </div>
+                        </div>
+                        <div class="article-link">
+                            <Link to="/growing-pains">
+                                <h1>GROWING <br></br> PAINS</h1>
+                            </Link>
+                            <div s-only="dsk" className="spacer"></div>
+                            <div className="hole-container">
+                                <Hole7 />
+                            </div>
+                        </div>
+                        <div class="article-link">
+                            <Link to="/ekfrasiksesta-osa-2">
+                                <h1>Ekfrasiksesta osa 2 <br></br>
+                                    <span className="smaller">Esityksen kuvittelemisesta  <br></br> ja muistamisesta</span></h1>
+                            </Link>
+                            <div s-only="dsk" className="spacer"></div>
+                            <div className="hole-container">
+                                <Hole8 />
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className="content-field s2020" ref={frame2}>
                         <div class="article-link">
                             <Link to="/practices-of-love-and-body">
                                 <h1>Practices of Love <br /> and Body</h1>
@@ -172,7 +324,7 @@ osa 3</h1>
 
                         </div>
                     </div>
-                    <div className="content-field s2021">
+                    <div className="content-field s2021" ref={frame3}>
                         <div class="article-link">
                             <Link to="/fluctuants">
                                 <h1>FLUCTUANTS</h1>
@@ -212,6 +364,50 @@ osa 3</h1>
                             </div>
                         </div>
 
+                    </div>
+                    <div className="content-field s2020" ref={frame4}>
+                        <div class="article-link">
+                            <Link to="/practices-of-love-and-body">
+                                <h1>Practices of Love <br /> and Body</h1>
+                            </Link>
+                            <div s-only="dsk" className="spacer"></div>
+                            <div className="hole-container">
+                                <Hole1 />
+                            </div>
+                        </div>
+                        <div class="article-link">
+                            <Link to="/rakkaudesta">
+                                <h1>Rakkaudesta <br />
+– sanasto tuleville vuosikymmenille</h1>
+                            </Link>
+
+                            <div s-only="dsk" className="spacer"></div>
+                            <div className="hole-container">
+                                <Hole4 />
+                            </div>
+                        </div>
+                        <div class="article-link">
+                            <Link to="/cloudbody">
+                                <h1>Cloudbody,<br />
+an Ecology of Dreams,<br />
+osa 3</h1>
+                            </Link>
+                            <div s-only="dsk" className="spacer"></div>
+                            <div className="hole-container">
+                                <Hole2 />
+                            </div>
+
+                        </div>
+                        <div class="article-link">
+                            <Link to="/tunnemylläkkähässäkkä">
+                                <h1>Tunnemylläkkähässäkkä</h1>
+                            </Link>
+                            <div s-only="dsk" className="spacer"></div>
+                            <div className="hole-container">
+                                <Hole3 />
+                            </div>
+
+                        </div>
                     </div>
                 </div>
             </div>
